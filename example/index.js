68cb54d56cf9efe1b6789352a0e96832acc1cd67
StyxWang <zrwangxdu@outlook.com>
2021-08-09 21:47:04 +0800

    add: 增加对bos触发器的支持

--- example/index.js
+++ example/index.js
@@ -1,13 +1,13 @@
 let resp = {
-    "version" : "2.0",
-    "response" : {
-        "outputSpeech": {
-            "type" : "PlainText",
-           "text" : "你好，世界！",
-        },
-        "shouldEndSession" : false
-    }
-}
+  version: '2.0',
+  response: {
+    outputSpeech: {
+      type: 'PlainText',
+      text: '你好，世界！',
+    },
+    shouldEndSession: false,
+  },
+};
 exports.handler = (event, context, callback) => {
-   callback(null, resp);
+  callback(null, resp);
 };
